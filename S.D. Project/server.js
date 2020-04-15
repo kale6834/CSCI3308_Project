2d5c76459b8c8a1227d60a4c0f6a4fd4205e832a
kale6834 <49416728+kale6834@users.noreply.github.com>
2020-04-14 19:16:23 -0600

    Fixed a lot of stuff with Yash. :)

--- S.D. Project/server.js
+++ S.D. Project/server.js
@@ -1,5 +1,6 @@
 var express = require('express'); //Ensure our express framework has been added
 var bodyParser = require('body-parser'); //Ensure our body-parser tool has been added
+var app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
@@ -7,13 +8,21 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 var pgp = require('pg-promise')();
 
 
-var dbConfig = process.env.DATABASE_URL; //connects to heroku database
+// var dbConfig = "postgres://fokxovrlpssdqp:f1752a76d748490250359ff737c6f26e08a07e47da5b765d0afdeb34edf96166@ec2-34-193-232-231.compute-1.amazonaws.com:5432/d23tobmqht1lf5"; //connects to heroku database";
 
-var mysql = pgp(dbConfig);
+const dbConfig = {
+    host: 'localhost',
+    port: 5432,
+    database: 'finalprojectdb',
+    user: 'kaleilewis',
+    password: 'password'
+};
+
+
+
+var db = pgp(dbConfig);
 var session = require('express-session');
 var path = require('path');
-
-var app = express();
 app.use(session({
     secret: 'secret',
     resave: true,
@@ -21,30 +30,38 @@ app.use(session({
 }));
 
 app.get('/', function(request, response) {
-    response.sendFile(path.join(__dirname + '/loginproject.html'));
+    response.sendFile(path.join(__dirname + '/Front_end/loginproject.html'));
 });
 
 app.post('/auth', function(request, response) {
     var username = request.body.username;
     var password = request.body.password;
+    console.log(username, password);
     if (username && password) {
-        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-            if (results.length > 0) {
-                request.session.loggedin = true;
-                request.session.username = username;
-                response.redirect('/home');
-            } else {
-                response.send('Incorrect Username and/or Password!');
-            }
-            response.end();
-        });
-    } else {
-        response.send('Please enter Username and Password!');
-        response.end();
+        var query = "SELECT * FROM players WHERE name ='" + username + "'AND password = '" + password + "'";
+        console.log(query);
+        db.any(query)
+            .then(function(results) {
+                if (results.length > 0) {
+                    request.session.loggedin = true;
+                    request.session.username = username;
+                    response.redirect('/homepage');
+
+                } else {
+                    response.send('Incorrect Username and/or Password!');
+                }
+                response.end();
+
+            })
+            .catch(function(err) {
+                // display error message in case an error
+                console.log('error', err);
+                response.send('error logging into database')
+            });
     }
 });
 
-app.get('/home', function(request, response) {
+app.get('/homepage', function(request, response) {
     if (request.session.loggedin) {
         response.send('Welcome back, ' + request.session.username + '!');
     } else {
@@ -53,4 +70,6 @@ app.get('/home', function(request, response) {
     response.end();
 });
 
-app.listen(process.env.PORT); //connects to heroku port
+// app.listen(process.env.PORT); //connects to heroku port
+
+app.listen(3000);
